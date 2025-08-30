c31bd7cd02d5fb10c997d7171aa41e6fd01e0e8b
hidehidehorie <b1a1s2k3e5t8hupro@gmail.com>
2025-08-30 17:07:17 +0900

    "ls"をコマンドラインから取得し一覧表示

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::env;
 
 struct Task {
     id: u32,
@@ -13,16 +14,23 @@ impl fmt::Display for Task {
 }
 
 fn main() {
-    // 追加・削除することを見据えてベクタにしている
-    let tasks = vec![
+    // コマンドライン引数を取得
+    let args: Vec<String> = env::args().collect();
+
+    // ひとまず引数が "ls" のときだけ処理
+    if args.len() > 1 && args[1] == "ls" {
+        let tasks = vec![
         // 勉強のためString型を３つ別々で書いてみる
         Task { id: 1, title: String::from("A") },
         Task { id: 2, title: "B".to_string() },
         Task { id: 3, title: "C".into() },
-    ];
+        ];
 
-    println!("=== TODO List ===");
-    for task in tasks {
-        println!("{}", task);
+        println!("=== TODO List ===");
+        for task in tasks {
+            println!("{}", task);
+        }
+    } else {
+        println!("一覧表示: cargo run -- ls");
     }
 }
